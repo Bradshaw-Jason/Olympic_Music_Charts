8dbb629deebe4f2899a72702c14fe70ff7b80d4f
Jason Bradshaw <jasoncbradshaw97@gmail.com>
2021-07-29 21:32:23 -0700

    added geojson to add markers to map

---
+++ assets/js/olympicsData.js
@@ -0,0 +1,66 @@
+var OlympicsData = {
+    "type": "FeatureCollection",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": [
+        {
+            "type": "Feature", "properties": {
+                "id": 0,
+                "Year": "2000",
+                "city": "Sydney",
+                "country": "Australia"
+            },
+            "geometry": { "type": "Point", "coordinates": [151.0634, -33.8471] }
+        },
+
+        {
+            "type": "Feature", "properties": {
+                "id": 1,
+                "Year": "2004",
+                "city": "Athens",
+                "country": "Greece"
+            },
+            "geometry": { "type": "Point", "coordinates": [23.7876, 38.0361] }
+        },
+
+        {
+            "type": "Feature", "properties": {
+                "id": 2,
+                "Year": "2008",
+                "city": "Bejing",
+                "country": "China"
+            },
+            "geometry": { "type": "Point", "coordinates": [116.3965, 39.9929] }
+        },
+
+        {
+            "type": "Feature", "properties": {
+                "id": 3,
+                "Year": "2012",
+                "city": "London",
+                "country": "United Kingdom"
+            },
+            "geometry": { "type": "Point", "coordinates": [-0.0166, 51.5387] }
+        },
+
+        {
+            "type": "Feature", "properties": {
+                "id": 4,
+                "Year": "2016",
+                "city": "Rio De Janerio",
+                "country": "Brazil"
+            },
+            "geometry": { "type": "Point", "coordinates": [-43.2302, -22.9121] }
+        },
+
+        {
+            "type": "Feature", "properties": {
+                "id": 5,
+                "Year": "2021",
+                "city": "Tokyo",
+                "country": "Japan"
+            },
+            "geometry": { "type": "Point", "coordinates": [139.7145, 35.6779] }
+        },
+    ]
+
+}
