16f7cd2d2e7848acc7af33044090c79d1a62716b
Jason Bradshaw <jasoncbradshaw97@gmail.com>
2021-08-02 20:48:27 -0700

    added functionality to update popup info with user data

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,13 @@ var athensButtonEl = $('#Host2')
 var beijingButtonEl = $('#Host3')
 var londonButtonEl = $('#Host4')
 var rioButtonEl = $('#Host5')
-var tokyoButtonEl =$('#Host6')
+var tokyoButtonEl = $('#Host6')
+
+
+//sets default values for user airport and date
+var userAirport = "JFK-sky";
+var userDate = "2021-08-04";
+
 
 
 //creates the map using leaflets API
@@ -27,9 +33,9 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 //creates a marker for each olympics stadium from geojson and adds to the map
 //calls the function that creats the popup content for each marker
-var stadiumMarkers = new L.geoJSON(OlympicsData,{
+var stadiumMarkers = new L.geoJSON(OlympicsData, {
     onEachFeature: createPopup,
-    pointToLayer: function (feature, latlng){
+    pointToLayer: function (feature, latlng) {
         return L.marker(latlng);
     }
 });
@@ -39,118 +45,116 @@ map.addLayer(stadiumMarkers);
 
 
 //this function generates the content for the popup at each marker
-function createPopup(feature, layer){
+function createPopup(feature, layer) {
 
-    var popupContent =""
+    var popupContent = ""
 
     getFlightInfo(feature.properties.airport)
-    .then(response => {
-        return response.json();
-    })
-    .then(data => {
-        console.log(data);
-        popupContent += "<h3>"+feature.properties.city+"</h3>";
-        if (data.Quotes.length === 0){
-            popupContent += "<p> No flights to this stadium could be found! please choose a different date of origin city</p>";
-        }
-        else{
-            // popupContent += "<p>"+data.Places[0].CityId+"</p>";
-        }
-        
-        layer.bindPopup(popupContent); 
-        
-    })
-    .catch(err => {
-        console.error(err);
-        popupContent += "<p>there was a problem</p>"
-        layer.bindPopup(popupContent); 
-    });
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            popupContent += "<h3>" + feature.properties.city + "</h3>";
+            if (data.Quotes.length === 0) {
+                popupContent += "<p> No flights to this stadium could be found! please choose a different date of origin city</p>";
+            }
+            else {
+                popupContent += "<p>" + data.Places[0].CityId + "</p>";
+            }
 
-    
+            layer.bindPopup(popupContent);
+
+        })
+        .catch(err => {
+            console.error(err);
+            popupContent += "<p>there was a problem</p>"
+            layer.bindPopup(popupContent);
+        });
+
+
 }
 
 
-function getFlightInfo(destination){
+function getFlightInfo(destination) {
 
     // return fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/?query=Stockholm", {
     // 	"method": "GET",
     // 	"headers": {
-	//     	"x-rapidapi-key": "74843f7863msh4827f7620e8ef19p122bb2jsn8896e6fc4cbc",
-	//     	"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
+    //     	"x-rapidapi-key": "74843f7863msh4827f7620e8ef19p122bb2jsn8896e6fc4cbc",
+    //     	"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
     // 	}
     // })
-    var flightURL = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/JFK-sky/"+destination+"/2021-07-31"
+    var flightURL = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/" + userAirport + "/" + destination + "/" + userDate
 
     return fetch(flightURL, {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "74843f7863msh4827f7620e8ef19p122bb2jsn8896e6fc4cbc",
-		"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
-	}
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-key": "74843f7863msh4827f7620e8ef19p122bb2jsn8896e6fc4cbc",
+            "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
+        }
     })
 
-// .then(response => {
-// 	console.log(response);
-// })
-// .catch(err => {
-// 	console.error(err);
-// });
+    // .then(response => {
+    // 	console.log(response);
+    // })
+    // .catch(err => {
+    // 	console.error(err);
+    // });
 
 };
 
-// zoomTestEl.on('click', function(){
-//     map.flyTo([-100,100], 5)
-// })
-
-// "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/SFO-sky/JFK-sky/2021-07-31"
-
 
 //search for a city ID
-// fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/US/USD/en-US/?query=seattle", {
-// 	"method": "GET",
-// 	"headers": {
-// 		"x-rapidapi-key": "74843f7863msh4827f7620e8ef19p122bb2jsn8896e6fc4cbc",
-// 		"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
-// 	}
-// })
-// .then(response => {
-// 	return response.json();
-// })
-// .then(data=> {
-// 	console.log(data);
-// })
+function searchUserAirport(userSearch) {
+    fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/US/USD/en-US/?query="+userSearch, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-key": "74843f7863msh4827f7620e8ef19p122bb2jsn8896e6fc4cbc",
+            "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
+        }
+    })
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+        })
+};
 
-sydneyButtonEl.on('click', function(){
+sydneyButtonEl.on('click', function () {
     map.flyTo([-33.8471, 151.0634], 15)
 })
 
-athensButtonEl.on('click', function(){
-    map.flyTo([38.0361,23.7876], 15)
+athensButtonEl.on('click', function () {
+    map.flyTo([38.0361, 23.7876], 15)
 })
 
-beijingButtonEl.on('click', function(){
-    map.flyTo([39.9929,116.3965], 15)
+beijingButtonEl.on('click', function () {
+    map.flyTo([39.9929, 116.3965], 15)
 })
 
-londonButtonEl.on('click', function(){
-    map.flyTo([51.5387,-0.0166], 15)
+londonButtonEl.on('click', function () {
+    map.flyTo([51.5387, -0.0166], 15)
 })
 
-rioButtonEl.on('click', function(){
+rioButtonEl.on('click', function () {
     map.flyTo([-22.9121, -43.2303], 15)
 
 })
 
-tokyoButtonEl.on('click', function(){
-    map.flyTo([35.6779,139.7145], 15)
+tokyoButtonEl.on('click', function () {
+    map.flyTo([35.6779, 139.7145], 15)
+
 })
 
-// .then(response => {
-// 	console.log(response);
-// })
-// .catch(err => {
-// 	console.error(err);
-// });
 
-};
+function userInput() {
+    userAirport = ""
+    userDate = ""
+    stadiumMarkers.eachLayer(function (layer) {
+        layer._popup.setContent(createPopup(layer.feature, layer))
+    });
+}
+
 
